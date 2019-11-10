256be9a3a53736924064dec7cee6cf69d063d2ef
Samuel Brucksch <samuel.b@freenet.de>
2019-11-10 20:56:32 +0100

    WIP

--- app/src/main/java/g4rb4g3/at/abrptransmitter/MainActivity.java
+++ app/src/main/java/g4rb4g3/at/abrptransmitter/MainActivity.java
@@ -4,46 +4,61 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-  public static final String PREFERENCES_NAME = "preferences";
-  public static final String PREFERENCES_TOKEN = "abrp_token";
-  public static final String PREFERENCES_TRANSMIT_DATA = "transmit_data";
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    public static final String PREFERENCES_NAME = "preferences";
+    public static final String PREFERENCES_TOKEN = "abrp_token";
+    public static final String PREFERENCES_TRANSMIT_DATA = "transmit_data";
 
-  @Override
-  protected void onResume() {
-    super.onResume();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    final SharedPreferences sp = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        final SharedPreferences sp = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
 
-    final TextView textViewToken = findViewById(R.id.tv_abrp_token);
-    textViewToken.setText(sp.getString(PREFERENCES_TOKEN, ""));
+        final TextView textViewToken = findViewById(R.id.tv_abrp_token);
+        textViewToken.setText(sp.getString(PREFERENCES_TOKEN, ""));
+        textViewToken.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-    final CheckBox cbTrsansmitData = findViewById(R.id.cb_transmit);
-    cbTrsansmitData.setChecked(sp.getBoolean(PREFERENCES_TRANSMIT_DATA, false));
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
-    Button btSave = findViewById(R.id.save);
-    btSave.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        SharedPreferences.Editor sped = sp.edit();
-        sped.putString(PREFERENCES_TOKEN, textViewToken.getText().toString());
-        sped.putBoolean(PREFERENCES_TRANSMIT_DATA, cbTrsansmitData.isChecked());
-        sped.commit();
+            @Override
+            public void afterTextChanged(Editable s) {
+                // save after every text change so we do not need save button
+                SharedPreferences.Editor sped = sp.edit();
+                sped.putString(PREFERENCES_TOKEN, textViewToken.getText().toString());
+                sped.commit();
+            }
+        });
 
-        Toast.makeText(getApplicationContext(), getText(R.string.saved), Toast.LENGTH_LONG).show();
-      }
-    });
-  }
+        final CheckBox checkBoxTransmitData = findViewById(R.id.cb_transmit);
+        checkBoxTransmitData.setChecked(sp.getBoolean(PREFERENCES_TRANSMIT_DATA, false));
+        checkBoxTransmitData.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // save after every toggle of checkbox so we do not need save button
+                SharedPreferences.Editor sped = sp.edit();
+                sped.putBoolean(PREFERENCES_TRANSMIT_DATA, isChecked);
+                sped.commit();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
---
+++ app/src/main/java/g4rb4g3/at/abrptransmitter/abrp/RoutePlan.java
@@ -0,0 +1,82 @@
+package g4rb4g3.at.abrptransmitter.abrp;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.TypeAdapters;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.Arrays;
+
+import cz.msebera.android.httpclient.Header;
+import g4rb4g3.at.abrptransmitter.ABetterRoutePlanner;
+import g4rb4g3.at.abrptransmitter.abrp.gson.DoubleTypeAdapter;
+import g4rb4g3.at.abrptransmitter.abrp.gson.GsonRoutePlan;
+
+public class RoutePlan {
+
+    public static final String ABETTERROUTEPLANNER_URL = "https://api.iternio.com/1/tlm/get_latest_plan?";
+
+    private static AsyncHttpClient asyncHttpClient;
+
+    private IRoutePlan listener;
+    private Gson gson;
+
+    public RoutePlan() {
+        asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.setTimeout(5000);
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapterFactory(TypeAdapters.newFactory(double.class, Double.class, new DoubleTypeAdapter()));
+        gson = builder.create();
+    }
+
+    public void addListener(IRoutePlan listener) {
+        this.listener = listener;
+    }
+
+    public void removeListener() {
+        this.listener = null;
+    }
+
+    private void parsePlanResponse(byte[] responseBody) {
+        try {
+            GsonRoutePlan gsonRoute = gson.fromJson(Arrays.toString(responseBody), GsonRoutePlan.class);
+            listener.planReady(gsonRoute);
+        } catch(Exception e) {
+            Log.d(ABetterRoutePlanner.TAG, "Failed to parse JSON: " + e.toString());
+            listener.planFailed();
+        }
+    }
+
+    public void requestPlan(String userToken) {
+        StringBuilder url = new StringBuilder(ABETTERROUTEPLANNER_URL)
+                .append(ABetterRoutePlanner.ABETTERROUTEPLANNER_URL_TOKEN)
+                .append("=").append(userToken)
+                .append("&")
+                .append(ABetterRoutePlanner.ABETTERROUTEPLANNER_URL_API_KEY)
+                .append("=")
+                .append(ABetterRoutePlanner.ABETTERROUTEPLANNER_API_KEY);
+
+        asyncHttpClient.get(url.toString(), new AsyncHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                parsePlanResponse(responseBody);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.e(ABetterRoutePlanner.TAG, String.valueOf(statusCode), error);
+                listener.planFailed();
+            }
+
+            @Override
+            public boolean getUseSynchronousMode() {
+                return false;
+            }
+        });
+    }
+}
